b832e0712a0a9fa9bf19f00c6ca342204bfce3ff
Vincent Filby <v@filby.ca>
2025-05-29 10:55:53 -0700

    Update Lily58 keymap: Replace quote and hyphen keys with parentheses in the number layer for improved punctuation access.

--- keyboards/splitkb/aurora/lily58/keymaps/filbar/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/filbar/keymap.c
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Numbers
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |SW_WIN|      |      |      |      |LOGOUT|                    |      |   "  |   '  |   -  |   _  | TRNS |
+ * |SW_WIN|      |      |      |      |LOGOUT|                    |      |   (  |      |   )  |   _  | TRNS |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |SW_APP|  ⌘Q  |  ⌘W  |      |      |      |                    |   *  |   7  |   8  |   9  |   +  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -179,7 +179,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_NUMBER] = LAYOUT(
-    SW_WIN,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LOGOUT,                     XXXXXXX, KC_DQUO, KC_QUOT, KC_MINS, KC_UNDS, _______,
+    SW_WIN,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LOGOUT,                     XXXXXXX, KC_LPRN, XXXXXXX, KC_RPRN, KC_UNDS, _______,
     SW_APP,  G(KC_Q), G(KC_W), XXXXXXX, XXXXXXX, XXXXXXX,                    KC_PAST, KC_7,    KC_8,    KC_9,    KC_PPLS, XXXXXXX,
     CW_TOGG, G(KC_A), G(KC_X), G(KC_C), G(KC_V), XXXXXXX,                    KC_PSLS, KC_4,    KC_5,    KC_6,    KC_MINS, KC_EQL,
     _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX, K_UNDO,   K_REDO,  KC_DOT,  KC_1,    KC_2,    KC_3,    KC_0,    XXXXXXX,
